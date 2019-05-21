8aa47200396b59b18763d1ac0d6ac8a24ff92955
kristina <kristina@flatclub.com>
2019-05-21 20:25:52 +0400

    First commit

---
+++ lessons/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using lessons.Payments;
+
+namespace lessons
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("hello world");
+
+            PayPal.Pay(100);
+            PayPal.Pay(50);
+        }
+    }
+}
