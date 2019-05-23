7dd6b4034feb34f935541feb7342a27a97728e3c
kristina <kristina@flatclub.com>
2019-05-23 19:23:23 +0400

    Third Commit

--- lessons.Payments/PayPal.cs
+++ lessons.Payments/PayPal.cs
@@ -8,6 +8,7 @@ namespace lessons.Payments
         {
             // Console.WriteLine("Pad " + amount + 'Successfully with PayPal");
             //Console.WriteLine("Pay {0} Successfully with PayPal, amount");
+
             Console.WriteLine( $"Pay ${amount} Successfully with PayPal ");
 
         }
