a37d9f012282a52dfd330f52dc4345565d8e95e4
kristina <kristina@flatclub.com>
2019-05-23 19:50:48 +0400

    Packages cgange

--- lessons.Payment/PayPal.cs
+++ lessons.Payment/PayPal.cs
@@ -4,12 +4,12 @@ namespace lessons.Payments
 {
     public class PayPal
     {
-       public static void Pay(int amount)
+        public static void Pay(int amount)
         {
             // Console.WriteLine("Pad " + amount + 'Successfully with PayPal");
             //Console.WriteLine("Pay {0} Successfully with PayPal, amount");
 
-            Console.WriteLine( $"Pay ${amount} Successfully with PayPal ");
+            Console.WriteLine($"Pay ${amount} Successfully with PayPal ");
 
         }
     }
